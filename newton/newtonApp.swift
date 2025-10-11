ede2d2cb434040bb88c4cf0548fbcdb6ee5feef6
newtondotcom <asphalt9fr@gmail.com>
2025-10-11 11:02:53 +0200

    Initial Commit

---
+++ newton/newtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  newtonApp.swift
+//  newton
+//
+//  Created by Robin Augereau on 11/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct newtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
